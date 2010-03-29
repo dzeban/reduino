4e69997f4afbebaf1fd3e66795ccc43b8d2bc666
dzeban <dzeban@eba290ab-e589-177a-c12d-ec6a73cc26a2>
2010-03-29 08:04:53 +0000

    blah-blah

--- actions/mainwindow_actions.cpp
+++ actions/mainwindow_actions.cpp
@@ -12,10 +12,12 @@ void MainWindow::on_actionOpenIcon_triggered()
     //	Call a file dialog and get name of file open
     QString opening_file = QFileDialog::getOpenFileName(this, tr("Open"),0 ,tr("Source code(*.pde *.h *.c);; All files(*)"));
 
-    // Create new tab
+    // Create new tab and upload file
     ui->tabWidget->addTab(new QPlainTextEdit(get_file_content(opening_file)), opening_file);
 
-    // Upload file to it
+
+
+
 
 
 }
