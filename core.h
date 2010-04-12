120c08adada1bf58efd2d81e943e6a1a09cacc1c
dzeban <dzeban@eba290ab-e589-177a-c12d-ec6a73cc26a2>
2010-04-12 08:32:00 +0000

    Added Makefile class stub.

--- core.h
+++ core.h
@@ -4,4 +4,17 @@
 QString get_file_content(QString filename);
 
 
+
+/*
+  Class for building and uploading code to board
+  */
+class Makefile
+{
+protected:
+    QString Makefile;	//class-member, that holds makefile
+
+public:
+
+};
+
 #endif // CORE_H
